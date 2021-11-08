a806181ba4468b945d07447dc78f8896f2234655
faboade oluwatosin <30568915+oluwatosinv@users.noreply.github.com>
2021-11-08 14:09:33 +0100

    reduc-tool-kit
    implementation of redux tool kit/query and cryptocurrencies UI

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,24 +1,75 @@
 import React from 'react'
 import millify from 'millify'
-import Link from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Row, Col, Typography, Statistic } from 'antd'
-import { Crytocurrencies } from '.'
+// import { Crytocurrencies } from '.'
 
-const {Title} = Typography
+import { useGetCryptosQuery } from '../services/crytoApi'
+import { Crytocurrencies, News } from '.'
+
+const { Title } = Typography
 
 const Homepage = () => {
-    return (
-        <>
-        <Title level={2} className='Heading'>Global Crypto Stats</Title>
-        <Row>
-            <Col span={12}><Statistic title='Total Crytocurrencies' value={5}/></Col>
-            <Col span={12}><Statistic title='Total Exchanges' value={5}/></Col>
-            <Col span={12}><Statistic title='Market Cap' value={5}/></Col>
-            <Col span={12}><Statistic title='Total 24h Volume' value={5}/></Col>
-            <Col span={12}><Statistic title='Total Markets' value={5}/></Col>
-        </Row>
-        </>
-    )
+  const { data, isFetching } = useGetCryptosQuery(10)
+
+  const globalStats = data?.data?.stats
+
+  if (isFetching) return 'Loading ....'
+
+  return (
+    <>
+      <Title level={2} className='Heading'>
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title='Total Crytocurrencies' value={globalStats.total} />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title='Total Exchanges'
+            value={millify(globalStats.totalExchanges)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title='Market Cap'
+            value={millify(globalStats.totalMarketCap)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title='Total 24h Volume'
+            value={millify(globalStats.total24hVolume)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title='Total Markets'
+            value={millify(globalStats.totalMarkets)}
+          />
+        </Col>
+      </Row>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>
+          Top 10 Crytocurrencies in the world
+        </Title>
+        <Title level={3} className='show-more'>
+          <Link to='/crytocurrencies'>Show more</Link>
+        </Title>
+      </div>
+      <Crytocurrencies simplified />
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>
+          Latest Cryptocurrency News
+        </Title>
+        <Title level={3} className='show-more'>
+          <Link to='/news'>Show more</Link>
+        </Title>
+      </div>
+      <News simplified />
+    </>
+  )
 }
 
 export default Homepage
---
+++ src/services/crytoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'ab52b248f3msh1eae89cb302bcaep15d08djsn7ec4d07450c5',
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+})
+
+export const { useGetCryptosQuery } = cryptoApi
